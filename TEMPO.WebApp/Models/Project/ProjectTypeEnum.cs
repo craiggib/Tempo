a9f96f1644fa2cea891ebffb3e74b757b6cb83e7
craig <cwillyg@hotmail.com>
2017-01-03 20:04:50 -0700

    time and materials view

---
+++ TEMPO.WebApp/Models/Project/ProjectTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEMPO.WebApp.Models.Project
+{
+    public enum ProjectTypeEnum
+    {
+        FixedPrice = 1,
+        TimeAndMaterials =2
+    }
+}
